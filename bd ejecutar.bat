de1a64392ddb801d4d75870689129b5fa13dfb42
Arthur Mazius <85359394+018100991k@users.noreply.github.com>
2022-08-10 11:37:12 -0500

    cambio

--- bd ejecutar.bat
+++ bd ejecutar.bat
@@ -2,7 +2,7 @@ echo Instalador de la base de datos Universidad
 echo Autor: Matheusz
 echo 8 de 8 de 2012
 sqlcmd -S. -E -i BDUniversidad.sql
-sqlcmd -S. -E -i BDUniversidadPA-A.sql
 sqlcmd -S. -E -i BDUniversidadPA-E.sql
+sqlcmd -S. -E -i BDUniversidadPA-A.sql
 echo Se ejecuto correctamente la base de datos
 pause
